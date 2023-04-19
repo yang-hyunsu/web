f8e02f70ac2fb9a9de8cb08d0b191b8d2f01a065
user <user@DESKTOP-COJ9858>
2023-04-19 14:17:52 +0900

    111

--- javaexp/src/javaexp/a09_inherit/A01_Basic.java
+++ javaexp/src/javaexp/a09_inherit/A01_Basic.java
@@ -12,7 +12,11 @@ public class A01_Basic {
 				변수를 호출해서 할당/사용할 때 사용된다.
 			2) this(), this(25), this("홍길동",25) :
 			 	생성자가 overloading으로 여러개 선언되어 있을 때,
-			 	다른 생성자를 호출해서 데이터를 할당할 때, 사용된다. 
+			 	다른 생성자를 호출해서 데이터를 할당할 때, 사용된다.
+			 	- 호출시, 반드시 생성자 선언 첫라인에서 호출하여야 한다.
+			ex) Product 클래스 선언하고, 기본생성자~매개변수3개 생성자를
+			 	선언하고 필드값 물건명, 가격, 갯수를 호출하게 하세요.
+			 	 
 		2. 상속관계에 있어서 this, super의 활용
 			 1) 필드, 생성자, 메서드 다 활용된다.
 			 2) 현재클래스와 상위클래스에 정의된 구성요소를 구분하여 호출할 때,
@@ -22,13 +26,39 @@ public class A01_Basic {
 			 3) 상위 선언된 메서드와 동일한 이름/매개변수가 동일하게 선언하고,
 			 	사용이 가능한데 이것을 메서드 overriding일 때,
 			 	상위 클래스의 메서드 : super.메서드(), 하위 클래스의 메서드
-			 	this.메서드()
-			 	
-			
+			 	this.메서드()			
 		*/
 		Person p01 = new Person("홍길동",25,"서울");
+		Product prod = new Product("사과",3000,2);
 	}
 }
+class Product{
+	String name;
+	int price;
+	int cnt;
+	public Product() {
+		System.out.println("객체 Product생성:"+this);
+	}
+	public Product(String name) {
+		this();
+		this.name = name;
+		System.out.println("매개변수1 - 객체 name 할당:"+name);
+	}
+	public Product(String name, int price) {
+		this(name);
+		this.price = price;
+		System.out.println("매개변수2 - 객체 price 할당:"+price);
+	}
+	public Product(String name, int price, int cnt) {
+		this(name,price);
+		this.cnt = cnt;
+		System.out.println("매개변수3 - 객체 cnt 할당:"+cnt);
+	}
+	
+	
+}
+
+
 // this()로 정의된 생성자 호출
 class Person{
 	String name;
@@ -38,8 +68,10 @@ class Person{
 		System.out.println("객체 생성:"+this);
 	}
 	public Person(String name) {
-		this(); // 생성자를 호출하면서 매개변수가 없는 생성자도 호출
+		this();
+		 // 생성자를 호출하면서 매개변수가 없는 생성자도 호출
 		this.name = name;
+//		this(); 호출시 에러 발생..
 		System.out.println("매개변수 1개");
 	}
 	public Person(String name, int age) {
--- javaexp/src/javaexp/a09_inherit/A02_Overriding.java
+++ javaexp/src/javaexp/a09_inherit/A02_Overriding.java
@@ -1,6 +1,7 @@
 package javaexp.a09_inherit;
 
 public class A02_Overriding {
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
