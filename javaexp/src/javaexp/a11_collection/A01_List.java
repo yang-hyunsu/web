88de3d0a8fd2b5b58ffb73512b376cf9d4810561
user <user@DESKTOP-COJ9858>
2023-04-21 11:44:09 +0900

    1111

---
+++ javaexp/src/javaexp/a11_collection/A01_List.java
@@ -0,0 +1,56 @@
+package javaexp.a11_collection;
+
+import java.util.ArrayList;
+
+public class A01_List {
+
+	public static void main(String[] args) {
+		// ctrl+shift+o : 자동 import
+		ArrayList<Person> plist = new ArrayList<Person>();
+		plist.add(new Person("홍길동",25,"서울"));
+		plist.add(new Person("김길동",27,"부산"));
+		plist.add(new Person("신길동",31,"대구"));
+		plist.add(new Person("오길동",20,"광주"));
+		plist.add(new Person("마길동",35,"제주"));
+		Person p01 = plist.get(1);
+		System.out.println("두번째 객체의 이름:"+p01.getName());
+		System.out.println("5번째 객체의 이름:"+plist.get(4).getName());
+		// ex) Student 반, 번호, 이름, 점수 속성 VO ArrayList에 할당
+		//     3개 할당 후, 마지막객체의 이름 호출, 첫번째 객체의 점수호출.
+		
+	}
+
+}
+class Person{
+	private String name;
+	private int age;
+	private String loc;
+	public Person() {
+	}
+	public Person(String name, int age, String loc) {
+		this.name = name;
+		this.age = age;
+		this.loc = loc;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getLoc() {
+		return loc;
+	}
+	public void setLoc(String loc) {
+		this.loc = loc;
+	}
+	
+}
+
+
