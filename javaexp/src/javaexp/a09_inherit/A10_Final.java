7f91f06a3b922a95ab6bdda71d800c2d58a3939a
user <user@DESKTOP-COJ9858>
2023-04-21 15:00:13 +0900

    1111

---
+++ javaexp/src/javaexp/a09_inherit/A10_Final.java
@@ -0,0 +1,58 @@
+package javaexp.a09_inherit;
+
+public class A10_Final {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		1. 필드에서 final
+			객체단위 final : 하나의 클래스서 생성되는 여러개 객체에
+				객체별로 변경하지는 상수..
+				class Person{
+					static final String KIND="황인";
+					final String name;
+					Person(String name){
+						this.name = name;
+					}
+				}
+				Person.KIND : 객체 생성없이도 사용할 수 있고,
+				Person.KIND="백인" : final이기에 변경할 수 없음
+					객체 공유 즉, class 상수.
+ 				// 객체별로 초기화는 가능하나
+				Person p01 = new Person("홍길동");
+				Person p02 = new Person("김길동");
+				p01.name="오길동"; // 한번 할당되 데이터는 변경하지 못함.
+				p02.name="마길동";
+				
+			객체공유클래스 final
+		2. class명 앞에 final : 더 이상 하위에 상속을 할 수 없는 클래스 선언
+		4. 메서드명 앞에 final : 더 이상 하위에서 재정의가 불가능 하는 메서드 선언.
+		
+		 * */
+	}
+}
+final class Riger{}
+//class NewSpec extends Riger{}
+class Book{
+	void showPrice() {
+		System.out.println("책의 가격 보여 주기~~");
+	}	
+	final void showAll() {
+		System.out.println("책 정보 보여 주기~~");
+	}
+}
+class SubBook extends Book{
+	@Override
+	void showPrice() {
+		// TODO Auto-generated method stub
+		super.showPrice();
+	}
+	
+}
+
+
+
+
+
+
+
---
+++ javaexp/src/javaexp/a09_inherit/A11_AbstractClass.java
@@ -0,0 +1,21 @@
+package javaexp.a09_inherit;
+
+public class A11_AbstractClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		# 추상 클래스(Abstract Class)
+		1. 추상 : 실체들 간에 공통되는 특성을 추출한 것을 말한다.
+			삼성, 현대, LG ==> 회사
+			새, 곤충, 물고기 ==> 동물
+		2. 추상클래스
+			실체 클래스들의 공통되는 필드와 메소드 정의한 클래스
+			추상적인 메서드를 선언하고, 하위클래스에서 이 메서드를 반드시(***)
+			재정의하게 한 것을 말한다. ==> 추상메서드가 하나라도 있으면 
+			추상클래스가 된다.
+		15:03~		
+		 * */
+	}
+
+}
