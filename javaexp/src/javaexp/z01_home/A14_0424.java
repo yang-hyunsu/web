6680011e81254ef3ec10745453caba9a90e8f073
user <user@DESKTOP-COJ9858>
2023-04-24 17:30:40 +0900

    1111

---
+++ javaexp/src/javaexp/z01_home/A14_0424.java
@@ -0,0 +1,21 @@
+package javaexp.z01_home;
+
+public class A14_0424 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+2023-04-19
+[1단계:개념] 1. 추상클래스와 일반 재정의 클래스의 차이점을 예제를 통해 기술하세요
+[1단계:개념] 2. Food를 추상클래스로 선언하고, name(음식종류), eat()공통메서드 @@을 먹다, taste()추상메서드로 다양한 맛을 출력
+		좋아하는음식 3가지를 실제클래스를 선언하여 출력하세요.
+[1단계:확인] 3. 위 내용을 DailyFoodSchedule로 1:다 관계로 먹는 사람과 일일 식사메뉴 3개를 Food타입 리스트로 처리할 수 있게 기능메서드 선언하여 출력하세요.
+[1단계:개념] 4. 인터페이스의 구성요소의 특징을 예제와 함께 선언하세요
+[1단계:확인] 5. FishingWay goFishing() 인터페이스를 선언하여, 상속받은 실제클래스를 2개를 선언하고, 실제클래스 Fisher에 이름과 인터페이스 FishingWay를 선언하고
+		addFishingSkill() 할당하게 하면  해당 어부가 낙시하는 goFishing()메서드를 호출하여 처리하세요.
+	
+
+		*/
+	}
+
+}
