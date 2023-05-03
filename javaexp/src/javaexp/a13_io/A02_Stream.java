a1d35d4f66333d50a870bd858718460785107cef
user <user@DESKTOP-COJ9858>
2023-05-03 14:35:00 +0900

    1111

--- javaexp/src/javaexp/a13_io/A02_Stream.java
+++ javaexp/src/javaexp/a13_io/A02_Stream.java
@@ -16,9 +16,14 @@ public class A02_Stream {
 		System.out.print("정답번호를 입력:");
 		try {
 			int numS = is.read();
+			// is.read(byte[] bytes) : 여러개의 문자를 입력받음.
+			// bytes : 입력된 문자의 배열을 코드값으로 배열에 저장.
+			// new String(bytes) : 여러개의 문자열 데이터를 확인..
 			OutputStream os = System.out;
 			System.out.println("입력된 정답번호:");
-			os.write(numS);
+			os.write(numS); // write를 통해서 char 한자를 출력.
+			// os.write(byte[])// 문자열을 출력..
+			os.flush();
 			char corChar = '3';
 			char inputChar = (char)numS;
 			System.out.println("정답:"+corChar+"번");
