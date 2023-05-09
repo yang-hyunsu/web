c7d15786b5e124ea7906039d2f8378315e26a070
user <user@DESKTOP-COJ9858>
2023-05-09 10:04:31 +0900

    111

--- frontWeb/database/a03_select_Basic.sql
+++ frontWeb/database/a03_select_Basic.sql
@@ -1,6 +1,10 @@
 /*
 # select sql의 명령문의 표준 형식
 1. 주로 서버에 데이터를 가져올 때, 활용된다.
+	client <====> sever
+				  초기 접근 권한/테이블 생성
+	요청/가져오기 select
+	전달만      입력/수정/삭제(insert, update, delete)  
 2. 기본형식
 	select * | 컬럼명 : 열단위로 나타날 데이터를 선택
 	from 테이블명 테이블명 별칭  : 대상 테이블명,테이블명 alias명
@@ -15,17 +19,18 @@ SELECT * FROM emp;
 SELECT deptno, ename, sal -- [열단위 선택] 컬럼단위로 열을 선택
 FROM emp
 WHERE sal > = 3000; --[행단위 필터링] sal가 3000이상인 경우만 검색
--- group 함수 처리
+-- group 함수 처리 (함수 진행시)
 SELECT deptno, sal
 FROM emp
 ORDER BY deptno; -- ORDER BY 컬럼명  특정 컬럼 기준으로 정렬
-SELECT deptno, avg(sal) msal  /* 그룹할 컬럼, 함수명(컬럼명)*/
+SELECT deptno, 
+		min(sal) msal, max(sal) mxsal, avg(sal) avgsal  /* 그룹할 컬럼, 함수명(컬럼명)*/
 FROM emp
 GROUP BY deptno;
 -- order by 컬럼명 desc|asc 정렬 기준 처리
 SELECT ename, sal
 FROM emp
-ORDER BY sal;
+ORDER BY sal desc;
 /*
 # column의 alias 활용
 1. select 컬럼명 as 별칭
@@ -33,7 +38,8 @@ ORDER BY sal;
 3. select 컬럼명 "공백이나 특수문자포함"
  * */
 SELECT ename AS name, sal salary, deptno 부서번호,
-	   empno "사원 번호", job "직책 번호"
+	   empno "사원 번호", job "직책 번호",
+	   sal "#급여@"
 FROM emp;
 --  ex) empno ==> no, ename ==> 사 원 명, mgr ==> 관리자번호
 --      로 표현하여 컬럼으로 출력하세요.
@@ -52,6 +58,12 @@ FROM emp;
 -- ex) 사원의 이름은 @@@ 입니다. 처리하세요..
 SELECT ename, '사원의 이름은 '||ename ||' 입니다.' msg
 FROM emp;
+SELECT a.*, sal + bonus
+from(
+	SELECT ename, sal, sal*0.2 bonus
+	FROM emp
+) a; -- subquery 활용..
+
 SELECT ename, sal, sal*0.2 "보너스(급여의 20%)"
 FROM emp;
 -- ex) 사원의 이름은 @@@이고 급여와 보너스(급여15%)의 합산은 @@@ 만원 입니다.
@@ -61,3 +73,5 @@ FROM emp;
 SELECT '사원의 이름은 '||ename ||'이고 급여와 보너스(급여15%)의'||
 ' 합산은 '|| (sal + sal*0.15) ||' 만원 입니다.' msg
 FROM emp;
+-- 문자열안에 연산을 처리할 때는 ()로 우선연산을 처리해서 활용한다.
+
