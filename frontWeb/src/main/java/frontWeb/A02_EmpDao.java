db45b8dd97ea45fa18c86ba77488eb6e8a5ed2b9
user <user@DESKTOP-COJ9858>
2023-05-18 16:44:04 +0900

    11111

---
+++ frontWeb/src/main/java/frontWeb/A02_EmpDao.java
@@ -0,0 +1,51 @@
+package frontWeb;
+
+import java.sql.*;
+
+public class A02_EmpDao {
+	//1. 필드선언(핵심 내장 객체)
+	// 	1) 연결 - 서버연결
+	private Connection con;
+	//  2) 대화 - 명령/입력  
+	private Statement stmt;
+	//  3) 결과 - 데이터를 가져오는 것
+	private ResultSet rs;
+	
+	//2. 기능 메서드 선언..
+	//	1) 사원 전체 정보 출력..
+	public void empListAll() {
+			// # 전체적으로 예외 처리.
+			// 연결
+			try {
+				con = DB.con();
+				// 대화할 내용
+				String sql = "SELECT * FROM emp02";
+				// 대화할 수 있는 객체는 연결(con)에 의해 생성
+				stmt = con.createStatement();
+				// 결과(ResultSet)
+				rs = stmt.executeQuery(sql);
+				int row=1;
+				while(rs.next()) { // 행단위로 이동 다음 행이 없을 떄까지
+					System.out.println("행번호:"+row++);
+				}
+				/*
+				rs.next() : 반복을 통해서 행단위로 커서를 위치시키는
+					다음 행이 데이터가 없으면 false 있으면 true
+				while(rs.next()){ // 다음 행이 없을 때까지 반복
+					System.out.println(cnt++)
+				}			
+				rs.get데이터유형("컬럼명")
+				 * 
+				 * */
+				// 자원해제				
+			} catch (SQLException e) {
+				System.out.println("DB 처리 예외:"+e.getMessage());
+			}
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		A02_EmpDao dao = new A02_EmpDao();
+		dao.empListAll();
+	}
+
+}
--- frontWeb/src/main/java/frontWeb/DB.java
+++ frontWeb/src/main/java/frontWeb/DB.java
@@ -17,6 +17,11 @@ public class DB {
 		}
 		// 2. 연결정보 처리
 		String info = "jdbc:oracle:thin:@localhost:1521:xe";
+		//    DriverManager : jdbc드라이버의 클래스 중에서
+		//    특정한 객체가 메모리에 로딩된 상황
+		///   접속정보를 매개변수로 계정과 비번을 받아서
+		//    특정 서버에 접속하고, 그 접속한 객체를 호출하는 곳에
+		//    리턴 처리..
 		con = DriverManager.getConnection(info,"scott","tiger");
 		System.out.println("DB 접속 성공");
 		return con;
