54895b7628bb53f2ca92c02ace1d0b8267a48de0
user <user@DESKTOP-COJ9858>
2023-05-24 11:18:09 +0900

    11111

--- frontWeb/src/main/java/frontWeb/A05_MemberDao.java
+++ frontWeb/src/main/java/frontWeb/A05_MemberDao.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Date;
+import java.util.Scanner;
 
 import frontWeb.vo.Member;
 
@@ -38,7 +38,7 @@ public class A05_MemberDao {
 					rs.getDate("regdate")
 				);
 			}
-			
+			// 
 		} catch (SQLException e) {
 			System.out.println("DB:"+e.getMessage());
 		}catch(Exception e) {
@@ -55,7 +55,31 @@ public class A05_MemberDao {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		A05_MemberDao dao = new A05_MemberDao();
+		Scanner sc = new Scanner(System.in);
+		System.out.println("#로그인 시작!!#");
+		while(true) {
+			
+			System.out.print("아이디를 입력:");
+			String id = sc.nextLine();
+			System.out.print("패스워드를 입력:");
+			String pass = sc.nextLine();
+			Member mem = dao.login(id, pass);
+			if(mem!=null) {
+				System.out.println("로그인 성공!");
+				System.out.println(mem.getName()+"님 환영합니다.");
+				break;
+			}else {
+				System.out.println("로그인 실패");
+				System.out.println(id+"는 인증계정이 아닙니다.");
+				System.out.print("로그인 계속 하시겠습니까?(Y/N):");
+				String isYN = sc.nextLine();
+				if(!isYN.equals("Y")) {
+					break;
+				}
+			}		
+		}
+		System.out.println("로그인 프로그램 종료!");
 	}
 
 }
