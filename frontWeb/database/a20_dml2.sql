0cb949060656805fdb6e381f96e8df923caa1253
user <user@DESKTOP-COJ9858>
2023-06-08 15:37:45 +0900

    1111

--- frontWeb/database/a20_dml2.sql
+++ frontWeb/database/a20_dml2.sql
@@ -130,3 +130,91 @@ UPDATE emp03
  SELECT *
   FROM emp03
  WHERE deptno = 30; -- 15:05~
+/*
+# 삭제 처리
+1. 데이터를 행단위를 삭제 처리하는 구문을 말한다.
+2. 기본 형식
+	delete
+	from 테이블명
+	where 조건처리
+3. subquery 삭제
+	delete
+	from 테이블
+	where 컬럼 = ( subquery)	
+# subquery를 활용한 where 조건의 조회/수정/삭제 	
+1. 단일/다중행 조건
+	delete
+	from 테이블
+	where 컬럼 = (결과가 1개의 데이터)
+	
+	select
+	from 테이블
+	where 컬럼 in (결과 2개 이상의 row 데이터)
+
+2. 하나의 컬럼/하나이상 컬럼 조건
+	delete
+	from 테이블
+	where (컬럼1, 컬럼2) = (결과가 1개의 데이터 - 컬럼이 다중)
+	
+	select
+	from 테이블
+	where (컬럼1, 컬럼2) in (결과 2개 이상의 row 데이터- 컬럼이 다중)	
+ * */
+SELECT * FROM EMP03;
+-- sal가 3000미만인 데이터를 삭제
+DELETE
+FROM emp03
+WHERE sal < 3000;
+-- ex) 입사일이 2사분기 데이터를 삭제..
+SELECT *
+FROM emp03
+WHERE to_char(hiredate,'Q')='2';
+delete
+FROM emp03
+WHERE to_char(hiredate,'Q')='2';
+CREATE TABLE emp09
+AS SELECT * FROM emp;
+SELECT * FROM emp09;
+-- 여러개의 row 데이터를 검색 처리하는 subquery
+-- 급여가 2000이상 사원 번호
+-- 사원 정보를 처리
+SELECT empno
+FROM emp09
+WHERE sal>=2000;
+
+SELECT *
+FROM emp09
+WHERE empno IN (
+	SELECT empno
+	FROM emp09
+	WHERE sal>=2000
+);
+-- 부서별 최고급여자 정보를 출력하고자 한다.
+SELECT deptno, max(sal)
+FROM emp09
+GROUP BY deptno;
+SELECT *
+FROM emp09
+WHERE (deptno, sal) IN (
+	SELECT deptno, max(sal)
+	FROM emp09
+	GROUP BY deptno
+);
+SELECT *
+FROM emp09;
+-- ex1) 직책별 최근 입사자들의 정보를 출력하세요
+-- ex2) 직책별 최저 급여인 사원 정보를 출력하세요
+SELECT *
+FROM emp09
+WHERE (job, hiredate) IN (
+	SELECT job, max(hiredate)
+	FROM emp09
+	GROUP BY job
+);
+SELECT *
+FROM emp09
+WHERE (job, sal) IN (
+	SELECT job, min(sal)
+	FROM emp09
+	GROUP BY job
+);
