8add4e86ccf42ca61307ec5f59bdf17235f126df
user <user@DESKTOP-COJ9858>
2023-07-05 16:53:26 +0900

    1111

--- backendWeb/src/main/java/backendWeb/a01_servlet/A12_AjaxControllerJob.java
+++ backendWeb/src/main/java/backendWeb/a01_servlet/A12_AjaxControllerJob.java
@@ -27,6 +27,7 @@ public class A12_AjaxControllerJob extends HttpServlet {
 	 */
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		// List까지 처리하고 
 	}
 
 }
