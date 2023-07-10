0b58bb73869fa51f6a926cd0d831e7248781b259
user <user@DESKTOP-COJ9858>
2023-07-10 17:03:27 +0900

    1111

--- springweb/src/main/java/springweb/a01_start/A01_StartController.java
+++ springweb/src/main/java/springweb/a01_start/A01_StartController.java
@@ -1,5 +1,14 @@
 package springweb.a01_start;
 
-public class A01_StartController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+// springweb.a01_start.A01_StartController
+@Controller
+public class A01_StartController {
+	// http://localhost:5080/springweb/start.do
+	@RequestMapping("/start.do")
+	public String start() {
+		return "WEB-INF\\views\\a01_start.jsp";
+	}
 }
