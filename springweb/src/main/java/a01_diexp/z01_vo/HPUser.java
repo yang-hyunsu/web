1a975de11b95aa5ee60fde6062b910e887a33d94
user <user@DESKTOP-COJ9858>
2023-07-12 16:19:19 +0900

    1111

---
+++ springweb/src/main/java/a01_diexp/z01_vo/HPUser.java
@@ -0,0 +1,36 @@
+package a01_diexp.z01_vo;
+
+public class HPUser {
+	private String name;
+	private HandPhone handPhone;
+	public HPUser() {
+		// TODO Auto-generated constructor stub
+	}
+	public HPUser(String name) {
+		this.name = name;
+	}
+	public void usingMyPhone() {
+		System.out.println("#핸드폰 사용#");
+		System.out.println("사용자:"+name);
+		if(handPhone!=null) {
+			System.out.println("#소유한 핸드폰 정보#");
+			System.out.println("폰번호:"+handPhone.getNumber());
+			System.out.println("폰제조사:"+handPhone.getCompany());
+		}else {
+			System.out.println("소유한 핸드폰이 없네요");
+		}
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public HandPhone getHandPhone() {
+		return handPhone;
+	}
+	public void setHandPhone(HandPhone handPhone) {
+		this.handPhone = handPhone;
+	}
+	
+}
---
+++ springweb/src/main/java/a01_diexp/z01_vo/HandPhone.java
@@ -0,0 +1,27 @@
+package a01_diexp.z01_vo;
+
+public class HandPhone {
+	private String number;
+	private String company;
+	public HandPhone() {
+		// TODO Auto-generated constructor stub
+	}
+	public HandPhone(String number, String company) {
+		this.number = number;
+		this.company = company;
+	}
+	public String getNumber() {
+		return number;
+	}
+	public void setNumber(String number) {
+		this.number = number;
+	}
+	public String getCompany() {
+		return company;
+	}
+	public void setCompany(String company) {
+		this.company = company;
+	}
+	
+
+}
