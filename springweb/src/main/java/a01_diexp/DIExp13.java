6e1b39c1fda4fb1d00a08d5141443f54a53490d2
user <user@DESKTOP-COJ9858>
2023-07-13 10:39:26 +0900

    1111

--- springweb/src/main/java/a01_diexp/DIExp13.java
+++ springweb/src/main/java/a01_diexp/DIExp13.java
@@ -18,7 +18,17 @@ public class DIExp13 {
 		System.out.println("컨테이너의 객체호출:"+obj);
 		Mart m01 = ctx.getBean("m01", Mart.class);
 		m01.buyList();
-		
+		/*
+		Member 아이디 패스워드 이름 권한 포인트
+		ShoppingMall 쇼핑몰명  Member
+			mlist로 할당.
+			showMemberList()로 해당 쇼핑몰의 
+				회원리스트(회원아이디, 이름, 권한, 포인트) 출력
+		1단계 Member 클래스 구성
+		2단계 ShoppingMall 클래스 구성
+		3단계 xml 컨테이너 선언
+		4단계 main() 호출 처리
+		 * */
 		
 		
 		ctx.close();
---
+++ springweb/src/main/java/a01_diexp/z01_vo/ShoppingMall.java
@@ -0,0 +1,44 @@
+package a01_diexp.z01_vo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ShoppingMall {
+	private String sname;
+	private List<Member> mlist;
+	public ShoppingMall() {
+		// TODO Auto-generated constructor stub
+	}
+	public ShoppingMall(String sname) {
+		this.sname = sname;
+		mlist = new ArrayList<Member>();
+	}
+	public void showMemberList() {
+		System.out.println(sname+"의 회원들");
+		System.out.println("아이디\t이름\t권한\t포인트");
+		if(mlist.size()>0) {
+			for(Member m:mlist) {
+				System.out.print(m.getId()+"\t");
+				System.out.print(m.getName()+"\t");
+				System.out.print(m.getAuth()+"\t");
+				System.out.print(m.getPoint()+"\n");
+			}
+		}else {
+			System.out.println("회원이 없네요!!");
+		}
+	}
+	public String getSname() {
+		return sname;
+	}
+	public void setSname(String sname) {
+		this.sname = sname;
+	}
+	public List<Member> getMlist() {
+		return mlist;
+	}
+	public void setMlist(List<Member> mlist) {
+		this.mlist = mlist;
+	}
+	
+	
+}
