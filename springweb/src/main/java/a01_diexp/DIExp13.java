187a0bbd76af0bd80580f99dd5ba8565728d6bf0
user <user@DESKTOP-COJ9858>
2023-07-13 09:37:25 +0900

    111

---
+++ springweb/src/main/java/a01_diexp/DIExp13.java
@@ -0,0 +1,21 @@
+package a01_diexp;
+
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class DIExp13 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 컨테이너 경로
+		String path="a01_diexp\\di13.xml";
+		AbstractApplicationContext ctx = 
+				new GenericXmlApplicationContext(path);
+		// DL(Dependency Lookup) 객체를 찾는 처리
+		Object obj = ctx.getBean("obj", Object.class);
+		System.out.println("컨테이너의 객체호출:"+obj);
+		ctx.close();
+		System.out.println("종료");
+	}
+
+}
