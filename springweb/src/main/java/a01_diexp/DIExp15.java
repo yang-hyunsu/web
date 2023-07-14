337b8c32ee3010b86ed08866b1a144589214f2e0
user <user@DESKTOP-COJ9858>
2023-07-14 16:43:41 +0900

    111

--- springweb/src/main/java/a01_diexp/DIExp15.java
+++ springweb/src/main/java/a01_diexp/DIExp15.java
@@ -3,6 +3,8 @@ package a01_diexp;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 
+import backendWeb.z01_vo.Calculator;
+import backendWeb.z01_vo.Dept;
 import backendWeb.z01_vo.Person;
 import backendWeb.z01_vo.Product;
 
@@ -23,6 +25,23 @@ public class DIExp15 {
 		System.out.println(p01.getName());
 		System.out.println(prod1.getName());
 		System.out.println(prod2.getName());
+		Dept d01 = ctx.getBean("d01", Dept.class);
+		System.out.println(d01.getDeptno());
+		System.out.println(d01.getDname());
+		System.out.println(d01.getLoc());
+		// d01 cal01
+		Calculator cal01 = ctx.getBean("cal01", Calculator.class);
+		Calculator cal02 = ctx.getBean("cal02", Calculator.class);
+		System.out.println();
+		System.out.print(cal01.getNum01());
+		System.out.print(cal01.getCal());
+		System.out.print(cal01.getNum02());
+		System.out.println("="+cal01.getTot());
+		System.out.print(cal02.getNum01());
+		System.out.print(cal02.getCal());
+		System.out.print(cal02.getNum02());
+		System.out.println("="+cal02.getTot());
+		// ex) 
 		
 		
 		ctx.close();
