21f9c023d0115016adba35363c301a9afba037d1
user <user@DESKTOP-COJ9858>
2023-07-17 11:16:01 +0900

    1111

---
+++ springweb/src/main/java/a01_diexp/z02_vo/Car.java
@@ -0,0 +1,38 @@
+package a01_diexp.z02_vo;
+
+import org.springframework.stereotype.Component;
+
+// a01_diexp.z01_vo.Car
+@Component
+public class Car {
+	private String kind;
+	private int cc;
+	private int maxVel;
+	public Car() {
+		// TODO Auto-generated constructor stub
+	}
+	public Car(String kind, int cc, int maxVel) {
+		this.kind = kind;
+		this.cc = cc;
+		this.maxVel = maxVel;
+	}
+	public String getKind() {
+		return kind;
+	}
+	public void setKind(String kind) {
+		this.kind = kind;
+	}
+	public int getCc() {
+		return cc;
+	}
+	public void setCc(int cc) {
+		this.cc = cc;
+	}
+	public int getMaxVel() {
+		return maxVel;
+	}
+	public void setMaxVel(int maxVel) {
+		this.maxVel = maxVel;
+	}
+	
+}
