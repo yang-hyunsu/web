5a6a355031a549a9700e155f617c21355eb9a31a
user <user@DESKTOP-COJ9858>
2023-07-17 12:20:42 +0900

    11111

--- springweb/src/main/java/a01_diexp/DIExp20.java
+++ springweb/src/main/java/a01_diexp/DIExp20.java
@@ -3,17 +3,24 @@ package a01_diexp;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 
+import a01_diexp.z04_vo.Code;
+import a01_diexp.z04_vo.Emp;
+
 public class DIExp20 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// 컨테이너 경로
-		String path="a01_diexp\\di10.xml";
+		String path="a01_diexp\\di20.xml";
 		AbstractApplicationContext ctx = 
 				new GenericXmlApplicationContext(path);
 		// DL(Dependency Lookup) 객체를 찾는 처리
-		Object obj = ctx.getBean("obj", Object.class);
-		System.out.println("컨테이너의 객체호출:"+obj);
+		Code obj1 = ctx.getBean("code", Code.class);
+		System.out.println("컨테이너의 객체호출:"+obj1);
+		Emp obj2 = ctx.getBean("emp", Emp.class);
+		System.out.println("컨테이너의 객체호출:"+obj2);
+		obj2.setEname("홍길동(사원)");
+		System.out.println(obj2.getEname());
 		ctx.close();
 		System.out.println("종료");
 	}
