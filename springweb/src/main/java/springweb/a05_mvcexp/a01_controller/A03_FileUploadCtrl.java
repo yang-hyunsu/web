d62bddd9cf1d49356bbb2e330291b4c0d97c1e19
user <user@DESKTOP-COJ9858>
2023-07-24 12:08:12 +0900

    111

--- springweb/src/main/java/springweb/a05_mvcexp/a01_controller/A03_FileUploadCtrl.java
+++ springweb/src/main/java/springweb/a05_mvcexp/a01_controller/A03_FileUploadCtrl.java
@@ -1,5 +1,6 @@
 package springweb.a05_mvcexp.a01_controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,8 +11,16 @@ import org.springframework.web.multipart.MultipartFile;
 public class A03_FileUploadCtrl {
 	@GetMapping("uploadExp03.do")
 	public String uploadExp03() {
+		System.out.println("공통경로:"+path);
+		System.out.println("데이터베이스:"+jdbc);
+		
 		return "WEB-INF\\views\\a05_mvcexp\\a05_fileupload.jsp";
 	}
+	@Value("${user.upload}")
+	private String path;
+	@Value("${jdbc.url}")
+	private String jdbc;
+	
 	@PostMapping("uploadExp03.do")
 	public String uploadExp03(@RequestParam("report01") MultipartFile[] mpfs,
 							  @RequestParam("title01") String title01 
@@ -20,6 +29,7 @@ public class A03_FileUploadCtrl {
 		for(MultipartFile mf:mpfs) {
 			System.out.println("파일:"+mf.getOriginalFilename());
 		}
+		
 		return "WEB-INF\\views\\a05_mvcexp\\a05_fileupload.jsp";
 	}	
 }
