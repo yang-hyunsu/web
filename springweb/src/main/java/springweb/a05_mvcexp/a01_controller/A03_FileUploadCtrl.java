38a422b63a43033df6198e313553b686ab335ff6
user <user@DESKTOP-COJ9858>
2023-07-24 14:39:16 +0900

    1111

--- springweb/src/main/java/springweb/a05_mvcexp/a01_controller/A03_FileUploadCtrl.java
+++ springweb/src/main/java/springweb/a05_mvcexp/a01_controller/A03_FileUploadCtrl.java
@@ -1,11 +1,15 @@
 package springweb.a05_mvcexp.a01_controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
+
+import springweb.a05_mvcexp.a02_service.A03_FileUploadService;
 
 @Controller
 public class A03_FileUploadCtrl {
@@ -21,14 +25,20 @@ public class A03_FileUploadCtrl {
 	@Value("${jdbc.url}")
 	private String jdbc;
 	
+	@Autowired
+	private A03_FileUploadService service;
+	
 	@PostMapping("uploadExp03.do")
 	public String uploadExp03(@RequestParam("report01") MultipartFile[] mpfs,
-							  @RequestParam("title01") String title01 
+							  @RequestParam("title01") String title01,
+							  Model d
 							  ) {
-		System.out.println("자료명:"+title01);
-		for(MultipartFile mf:mpfs) {
-			System.out.println("파일:"+mf.getOriginalFilename());
-		}
+//		System.out.println("자료명:"+title01);
+//		for(MultipartFile mf:mpfs) {
+//			System.out.println("파일:"+mf.getOriginalFilename());
+//		}
+		d.addAttribute("msg", service.upload(mpfs, title01));
+		
 		
 		return "WEB-INF\\views\\a05_mvcexp\\a05_fileupload.jsp";
 	}	
