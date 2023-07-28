35e9b0ff5c45c6cc5eb146e1a93b2f83502ed5d5
user <user@DESKTOP-COJ9858>
2023-07-28 10:38:05 +0900

    111

--- springweb/src/main/java/springweb/a05_mvcexp/a03_dao/A07_RegDao.java
+++ springweb/src/main/java/springweb/a05_mvcexp/a03_dao/A07_RegDao.java
@@ -2,9 +2,11 @@ package springweb.a05_mvcexp.a03_dao;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 import springweb.a05_mvcexp.z01_vo.Region;
 
@@ -17,6 +19,17 @@ public interface A07_RegDao {
 		String region_name);
 	@Insert("INSERT INTO regions01 values(\r\n"
 			+ "			#{region_id},#{region_name})")
-	public int insertRegion(Region insert);			 
+	public int insertRegion(Region insert);
+	@Select("	select *\r\n"
+			+ "	from regions01\r\n"
+			+ "	where  region_id = #{region_id}")
+	public Region getReg(@Param("region_id") int region_id);
+	@Update("	update regions01\r\n"
+			+ "	   set region_name = #{region_name}\r\n"
+			+ "	where region_id = #{region_id} ")
+ 	public int updateReg(Region upt);
+	@Delete("	delete regions01\r\n"
+			+ "	where region_id = #{region_id}")
+	public int deleteReg(@Param("region_id") int region_id);
 
 }
