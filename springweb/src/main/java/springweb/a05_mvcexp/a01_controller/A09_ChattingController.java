c1009644388cec066d857e996dc2591f5a410876
user <user@host.docker.internal>
2023-08-11 14:14:40 +0900

    1111

--- springweb/src/main/java/springweb/a05_mvcexp/a01_controller/A09_ChattingController.java
+++ springweb/src/main/java/springweb/a05_mvcexp/a01_controller/A09_ChattingController.java
@@ -1,7 +1,13 @@
 package springweb.a05_mvcexp.a01_controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import springweb.a06_util.ChatHandler;
 
 @Controller
 public class A09_ChattingController {
@@ -9,5 +15,12 @@ public class A09_ChattingController {
 	@GetMapping("chatting.do")
 	public String chatting() {
 		return "WEB-INF\\views\\a05_mvcexp\\a12_chatting.jsp";
+	}
+	@Autowired
+	private ChatHandler chatHandler;
+	// ajax로 현재 접속한 인원 확인
+	@GetMapping("getChatMem.do")
+	public ResponseEntity<List<String>> getChatMem() {
+		return ResponseEntity.ok(chatHandler.getIds());
 	}
 }
