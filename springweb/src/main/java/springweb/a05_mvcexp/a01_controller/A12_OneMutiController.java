1320a2635d21f67aac17d755b03152317c851ceb
ssangyoung1200 <ssangyoung1200@gmail.com>
2023-09-04 14:30:43 +0900

    111

--- springweb/src/main/java/springweb/a05_mvcexp/a01_controller/A12_OneMutiController.java
+++ springweb/src/main/java/springweb/a05_mvcexp/a01_controller/A12_OneMutiController.java
@@ -16,14 +16,22 @@ public class A12_OneMutiController {
 	@RequestMapping("insertForm.do")
 	public String addForm(Product ins) {
 		
+		// service.insertProdu(ins)
+		
+		// 서비스단에서 
+		// 반복처리하는 dao insert 처리
 		System.out.println("# 등록할 정보 #");
 		System.out.println(ins.getPname());
 		System.out.println("방의갯수:"+ins.getName().length);
+		
 		for(int idx=0;idx<ins.getName().length;idx++) {
 			System.out.print(ins.getName()[idx]+"\t");
 			System.out.print(ins.getPrice()[idx]+"\t");
 			System.out.println(ins.getCnt()[idx]);
+			// dao.insertRoom(ins.getProd_id(), ins.getRoom_id()[idx],.....)
 		}
+		// dao.insertProduct(.....  )
+		
 		
 		return "WEB-INF\\views\\a05_mvcexp\\a15_multiForm.jsp";
 	}
